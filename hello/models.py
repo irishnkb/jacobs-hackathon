b7259e00b196d708c548a8849374068e34b7d9c4
Jimin Lee <jiminlee@FBI-SurveillanceVan.local>
2021-11-20 23:38:49 +0100

    7th? revision

--- hello/models.py
+++ hello/models.py
@@ -3,3 +3,8 @@ from django.db import models
 class Item(models.Model):
     name = models.CharField(max_length=1000)
     availability = models.BooleanField()
+
+
+record = Item(name="jimmy")
+record = Item(availability=0)
+record.save()
