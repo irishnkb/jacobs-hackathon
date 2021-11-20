9083df63e2617e3a8a379828659dbd7627672bd5
Jimin Lee <jiminlee@FBI-SurveillanceVan.local>
2021-11-20 22:46:08 +0100

    6th? revision

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -16,5 +16,4 @@ urlpatterns = [
     path("db/", hello.views.db, name="db"),
     path("my_team_is_cool", hello.views.hello_world),
     path("admin/", admin.site.urls),
-    path("items", hello.views.items),
 ]
