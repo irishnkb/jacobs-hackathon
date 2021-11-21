b34e073c9d5651e0768f4925862fee9c923b1e67
SkryzhadlovskaA <a.skryzhadlovska@jacobs-university.de>
2021-11-21 08:00:31 +0100

    feat: trial and error

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 admin.autodiscover()
 
-import hello.views
+import static.hello.views
 
 # To add a new path, first import the app:
 # import blog
@@ -14,11 +14,11 @@ import hello.views
 # Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
 
 urlpatterns = [
-    path("", hello.views.index, name="index"),
-    path("items", hello.views.items, name="items"),
-    path("db/", hello.views.db, name="db"),
-    path("my_team_is_cool", hello.views.hello_world),
+    path("", static.hello.views.index, name="index"),
+    path("items", static.hello.views.items, name="items"),
+    path("db/", static.hello.views.db, name="db"),
+    path("my_team_is_cool", static.hello.views.hello_world),
     path("admin/", admin.site.urls),
-    path("sports", hello.views.sport),
-    path("update_db/", hello.views.update_db)
+    path("sports", static.hello.views.sport),
+    path("update_db/", static.hello.views.update_db)
 ]
