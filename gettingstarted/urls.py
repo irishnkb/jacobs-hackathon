29e45182bc60a7fb49c9d38b69d0184fcac1e8ef
SkryzhadlovskaA <a.skryzhadlovska@jacobs-university.de>
2021-11-21 10:59:31 +0100

    true/false

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -16,6 +16,7 @@ import hello.views
 urlpatterns = [
     path("", hello.views.index, name="index"),
     path("items", hello.views.items, name="items"),
+    path("search", hello.views.search, name="search"),
     path("db/", hello.views.db, name="db"),
     path("my_team_is_cool", hello.views.hello_world),
     path("sports", hello.views.sport),
