0b4a9e5a45612132758d3d71cf02ae29e3ab0060
SkryzhadlovskaA <a.skryzhadlovska@jacobs-university.de>
2021-11-21 10:19:13 +0100

    fix: import models

--- hello/views.py
+++ hello/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from models import Item,Timing
+from hello.models import Item,Timing
 
 # Create your views here.
 def index(request):
