45a32d03b812807af4a8c2b7d9c1ceddba9ab3db
Akhill Dhileep <akhilldj@gmail.com>
2021-11-21 06:31:55 +0100

    date and time model made

--- hello/migrations/0002_item.py
+++ hello/migrations/0002_item.py
@@ -18,26 +18,29 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=1000)),
                 ('availability', models.BooleanField()),
-            ],
-        ),
-    ]
-
-class Migration2(migrations.Migration):
-
-    initial = True
-
-    dependencies = [
-        ('hello', '0001_initial'),
-    ]
-
-    operations = [
-        migrations.CreateModel(
-            name='Time',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('day', models.CharField(max_length=1000)),
                 ('date', models.DateField()),
                 ('time', models.TimeField()),
             ],
         ),
     ]
+
+# class Migration2(migrations.Migration):
+
+#     initial = True
+
+#     dependencies = [
+#         ('hello', '0001_initial'),
+#     ]
+
+#     operations = [
+#         migrations.CreateModel(
+#             name='Time',
+#             fields=[
+#                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+#                 ('day', models.CharField(max_length=1000)),
+#                 ('date', models.DateField()),
+#                 ('time', models.TimeField()),
+#             ],
+#         ),
+#     ]
--- hello/models.py
+++ hello/models.py
@@ -3,10 +3,13 @@ from django.db import models
 class Item(models.Model):
     name = models.CharField(max_length=1000)
     availability = models.BooleanField() 
-
-class Timmings(models.Model):
     day = models.CharField(max_length=1000),
     date = models.DateField(),
     time = models.TimeField(),
+
+# class Timmings(models.Model):
+#     day = models.CharField(max_length=1000),
+#     date = models.DateField(),
+#     time = models.TimeField(),
 #record = Item(name="jimmy")
 #record.save()
